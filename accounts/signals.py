2d37ffd3ed8a21bc9bc5a04c25ca2dba933ec52d
adhish1478 <adhish.aravind7@gmail.com>
2025-05-08 22:28:43 +0530

    dual tables for recruiters and job seekers: all operations working fine

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,12 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Profile
+from .models import CustomUser, Profile, RecruiterProfile, JobSeekerProfile
 
 @receiver(post_save, sender= CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user= instance)
+        profile= Profile.objects.create(user= instance)
+
+        if instance.role == 'recruiter':
+            RecruiterProfile.objects.create(profile= profile)
+        elif instance.role == 'job_seeker':
+            JobSeekerProfile.objects.create(profile= profile)
 
 @receiver(post_save, sender= CustomUser)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserProfileView
 
 
 urlpatterns=[
     path('register/', RegisterView.as_view(), name= 'register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name= 'token_refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name= 'token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
