304767a66f11ffa7780cf49d1fa0ed0313b80be8
adhish1478 <adhish.aravind7@gmail.com>
2025-05-09 19:19:34 +0530

    crud operations for recruiter

--- jobs/permissions.py
+++ jobs/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission
 class IsRecruiter(BasePermission):
     def has_permission(self, request, view):
         # Allow access only to authenticated users
-        return request.user.is_authenticated and request.user.is_recruiter
+        return request.user.is_authenticated and request.user.role == 'recruiter'
     
     def has_object_permission(self, request, view, obj):
         # Allow access only to the recruiter who created the job
--- jobs/urls.py
+++ jobs/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import JobListCreateView, JobDetailView
 
 urlpatterns=[
-    
+    path('recruiter/jobs/', JobListCreateView.as_view(), name='recruiter-jobs'),
+    path('recruiter/jobs/<int:pk>/', JobDetailView.as_view(), name='recruiter-job-detail'),
 ]
