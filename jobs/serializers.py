bfaa1990388d90400d9650bedc13ef8882211769
adhish1478 <adhish.aravind7@gmail.com>
2025-05-13 17:52:07 +0530

    min and max exp added to job model, added filter system to the job view

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -7,3 +7,11 @@ class JobSerializer(serializers.ModelSerializer):
         fields= '__all__'
         read_only_fields= ('recruiter', 'created_at', 'updated_at')
         
+class FilteredJobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Job
+        fields= [
+            'id', 'title', 'description', 'location',
+            'skills_required', 'salary_range', 'min_experience',
+            'created_at'
+        ]
