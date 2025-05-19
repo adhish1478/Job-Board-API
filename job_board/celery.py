73e72f78b54fb2576fc5b87c2308cd860c903761
adhish1478 <adhish.aravind7@gmail.com>
2025-05-20 03:38:15 +0530

    celery and redis completely finished for verification mail

--- job_board/celery.py
+++ job_board/celery.py
@@ -4,5 +4,10 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_board.settings')
 
 app= Celery('job_board')
+
+# ✅ Explicitly set broker (sometimes settings aren't picked up otherwise)
+app.conf.broker_url = 'redis://localhost:6379/0'
+
 app.config_from_object('django.conf:settings', namespace= 'CELERY')
 app.autodiscover_tasks()
+
