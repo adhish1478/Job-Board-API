1119cd59600a2ab2ced2dec253f3436bcf89f3ab
adhish1478 <adhish.aravind7@gmail.com>
2025-06-25 14:28:56 +0530

    highlight already applied jobs

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,9 +1,9 @@
-const host= "http://56.228.30.131:8000/api" // Base URL for the API
+const host = "http://56.228.30.131:8000/api" // Base URL for the API
 
 async function fetchJobs() {
     try {
-        const token= localStorage.getItem('token');
-        const response= await fetch(host + "/jobs/", {
+        const token = localStorage.getItem('token');
+        const response = await fetch(host + "/jobs/", {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
@@ -11,102 +11,110 @@ async function fetchJobs() {
             }
         });
 
-        if(!response) throw new Error("Failed to fetch Jobs!");
+        if (!response) throw new Error("Failed to fetch Jobs!");
 
-        const jobs= await response.json()
-        renderJobs(jobs);
+        const jobs = await response.json();
+        const appliedJobs = await fetchAppliedJobs(token) //for rendering both jobs as well as applied jobs
+        renderJobs(jobs, appliedJobs)
 
-    } catch(err) {
+
+    } catch (err) {
         console.error("Error loading jobs: ", err);
         alert("Could not load jobs, Please try again later.")
     }
 }
 
-function renderJobs(jobs) {
-    const jobList= document.getElementById('jobList');
-    jobList.innerHTML= "";
+function renderJobs(jobs, appliedJobIds) {
+    const jobList = document.getElementById('jobList');
+    jobList.innerHTML = "";
 
-    const jobArray= jobs.results || jobs; // Handles both paginated and raw list
+    const jobArray = jobs.results || jobs; // Handles both paginated and raw list
 
     if (!jobArray.length) {
-        jobList.innerHTML= "<p>No jobs to display!</p>";
+        jobList.innerHTML = "<p>No jobs to display!</p>";
         return;
     }
 
     jobArray.forEach(job => {
-        const div= document.createElement('div');
-        div.className= 'job-block'
-        div.innerHTML= `
+        const isApplied = appliedJobIds.includes(job.id);
+        const applyButton = isApplied ?
+            `<button class="btn btn-secondary" disabled>Already Applied</button>` :
+            `<button class= "btn btn-success float-right" onclick="applyToJob(${job.id})">Apply</button>` // 4 lines of code for already applied grey style
+
+
+        const div = document.createElement('div');
+        div.className = isApplied ? 'job-block applied' : 'job-block';
+        div.innerHTML = `
             <h5>${job.title}</h5>
             <p>${job.description}</p>
             <p><strong>Location:</strong> ${job.location}</p>
-            <p><strong>Skills:</strong> ${job.skills ? job.skills_required.split(",").join(", ") : "Not specified"}</p>
+            <p><strong>Skills:</strong> ${job.skills_required ? job.skills_required.split(",").join(", ") : "Not specified"}</p>
             <p><strong>Experience:</strong> ${job.min_experience} years</p>
-            <button class="btn btn-success float-right" onclick="applyToJob(${job.id})">Apply</button>
+            ${applyButton}
         `;
         jobList.appendChild(div)
     });
 }
 
 async function applyToJob(jobId) {
-    const token= localStorage.getItem('token');
-    selectedJobId= jobId;
+    const token = localStorage.getItem('token');
+    selectedJobId = jobId;
 
     try {
-        const res= await fetch(host + '/profile/' ,{
-            headers:{
+        const res = await fetch(host + '/profile/', {
+            headers: {
                 'Authorization': 'Bearer ' + token
             }
         });
 
-        const profile= await res.json();
-        const hasResume= profile.resume !== null && profile.resume !== "";
+        const profile = await res.json();
+        const hasResume = profile.resume !== null && profile.resume !== "";
 
-        const modalBody= document.getElementById('modalBodyContent');
-        const resumeInput= document.getElementById('resumeInput');
-        resumeInput.hidden= true;
+        const modalBody = document.getElementById('modalBodyContent');
+        const resumeInput = document.getElementById('resumeInput');
+        resumeInput.hidden = true;
 
         if (hasResume) {
-            modalBody.innerHTML=`
+            modalBody.innerHTML = `
             <p>You already have a resume uploaded. What would you like to do?</p>
             <button type="button" class="btn btn-outline-primary me-2" onclick="useExistingResume()">Use Existing Resume</button>
             <button type="button" class="btn btn-outline-secondary" onclick="uploadNewResume()">Upload New Resume</button>
             `;
         } else {
-            modalBody.innerHTML= `<p>You haven’t uploaded a resume yet. Please upload one to apply.</p>`;
-            resumeInput.hidden= false;
+            modalBody.innerHTML = `<p>You haven’t uploaded a resume yet. Please upload one to apply.</p>`;
+            resumeInput.hidden = false;
         }
 
         new bootstrap.Modal(document.getElementById('resumeModal')).show()
 
-    } catch(err) {
+    } catch (err) {
         alert("Couldn't load profile, please login again");
         console.log('resume check failed!')
     }
 }
 
 function uploadNewResume() {
-    document.getElementById('resumeInput').hidden= false;
+    document.getElementById('resumeInput').hidden = false;
 }
 
 function useExistingResume() {
-    document.getElementById('resumeInput').hidden= true;
+    document.getElementById('resumeInput').hidden = true;
 }
 
 // Handle modal form submit
-document.getElementById('resumeForm').addEventListener("submit", async function (e) {
+document.getElementById('resumeForm').addEventListener("submit", async function(e) {
     e.preventDefault();
-    
-    const token= localStorage.getItem('token')
-    const formData= new FormData();
-    const fileInput= document.getElementById('resumeInput');
+
+    const token = localStorage.getItem('token')
+    const formData = new FormData();
+    const fileInput = document.getElementById('resumeInput');
 
     if (!fileInput.hidden && fileInput.files.length > 0) {
         formData.append("resume", fileInput.files[0]);
     }
 
     try {
-        const response= await fetch(`${host}/apply/${selectedJobId}/`, {
+        const response = await fetch(`${host}/apply/${selectedJobId}/`, {
             method: 'POST',
             headers: {
                 Authorization: "Bearer " + token
@@ -114,12 +122,12 @@ document.getElementById('resumeForm').addEventListener("submit", async function 
             body: formData
         });
 
-        if(response.ok) {
+        if (response.ok) {
             alert("Application submitted succesfully!")
             bootstrap.Modal.getInstance(document.getElementById('resumeModal')).hide();
         } else {
-            const data= await response.json();
-            alert("Error: "+ (data.detail || "Couldn't apply."))
+            const data = await response.json();
+            alert("Error: " + (data.detail || "Couldn't apply."))
         }
     } catch (err) {
         alert("Error applying to job!");
@@ -127,6 +135,22 @@ document.getElementById('resumeForm').addEventListener("submit", async function 
     }
 });
 
-fetchJobs();
+// For checking applied jobs and greying out buttons
+let appliedJobIds = []
 
-// resume popup and
+async function fetchAppliedJobs(token) {
+    try {
+        const res = await fetch(host + '/my-applications/', {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        });
+        const data = await res.json();
+        return data.map(app => app.job)
+    } catch (err) {
+        console.error("Failed to fetch applied jobs", err);
+        return [];
+    }
+}
+
+fetchJobs();
